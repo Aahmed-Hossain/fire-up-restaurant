3f1065d1d57d8e93852e16dd934957ff5f381082
Aahmed-Hossain <aahmdctec@gmail.com>
2024-03-13 03:24:43 +0600

    updated the added food page for md, sm and lg screen

--- src/components/MyAddedFoods/AddedFoodList.jsx
+++ src/components/MyAddedFoods/AddedFoodList.jsx
@@ -1,66 +1,86 @@
 /* eslint-disable react/prop-types */
-import { Link } from 'react-router-dom';
-import edit from '../../assets/images/edit.jpg'
-import useAxios from '../../hooks/useAxios';
-import swal from 'sweetalert';
+import { Link } from "react-router-dom";
+import edit from "../../assets/images/edit.jpg";
+import useAxios from "../../hooks/useAxios";
+import swal from "sweetalert";
 
-const AddedFoodList = ({addedFood,refetch}) => {
-    const {_id, image, quantity ,price ,date, food_name, email,customerName } = addedFood;
-const axiosHook = useAxios();
-    const handleDelete = id => {
-      swal({
-        title: "Are you sure?",
-        text: "Once deleted, you will not be able to recover this.!",
-        icon: "warning",
-        buttons: true,
-        dangerMode: true,
-      })
-      .then((willDelete) => {
-        if (willDelete) {
-          swal("Yes! Your Food has been deleted!", {
-            icon: "success",
-          });
-          axiosHook.delete(`/allFoods/${id}`)
-          .then(res => {
-            console.log(res.data);
-            if(res.data.deleteCount){
-                alert('deleted succesfully')
-            }
-            refetch();
-        })
-        } else {
-          swal("Your Food not deleted!");
-        }
-      });
-    }
-    return (
-        <div className="w-full md:w-11/12 lg:w-9/12 mx-auto ">
+const AddedFoodList = ({ addedFood, refetch }) => {
+  const { _id, image, quantity, price, date, food_name, email,  } =
+    addedFood;
+  const axiosHook = useAxios();
+  const handleDelete = (id) => {
+    swal({
+      title: "Are you sure?",
+      text: "Once deleted, you will not be able to recover this.!",
+      icon: "warning",
+      buttons: true,
+      dangerMode: true,
+    }).then((willDelete) => {
+      if (willDelete) {
+        swal("Yes! Your Food has been deleted!", {
+          icon: "success",
+        });
+        axiosHook.delete(`/allFoods/${id}`).then((res) => {
+          console.log(res.data);
+          if (res.data.deleteCount) {
+            alert("deleted succesfully");
+          }
+          refetch();
+        });
+      } else {
+        swal("Your Food not deleted!");
+      }
+    });
+  };
+  return (
+    <div className="w-full md:w-11/12 lg:w-9/12 mx-auto ">
       <div className="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-3 items-center justify-between gap-2  border border-slate-200 rounded-xl mb-4 px-0 md:px-2 lg:px-2 ">
         {/* left container */}
-        <div className='flex items-center gap-3'>
-        <button onClick={()=>handleDelete(_id)} className=" btn-circle btn-sm opacity-50 ml-2 hover:opacity-70 bg-black text-white">
-          X
-        </button>
-          <img className="w-[15rem] rounded-xl flex items-start" src={image} alt="" />
-        <div>
-          <p className='text-xl font-semibold text-[#FF3811]'>{food_name}</p>
-          <p className='text-[#A2A2A2]'>Email: {email}</p>
-          <p className='text-[#A2A2A2]'>Name: {customerName}</p>
+        <div className="flex items-center justify-center gap-3">
+          <button
+            onClick={() => handleDelete(_id)}
+            className="btn-circle btn-sm opacity-50 hover:opacity-70 bg-black text-white hidden md:block lg:block px-3 ml-1"
+          >
+            X
+          </button>
+          <img
+            className="lg:w-[15rem] md:w-[11rem] w-full rounded-xl flex items-start"
+            src={image}
+            alt=""
+          />
+        </div>
+
+           {/* info container*/}
+           <div className='px-2'>
+          <p className='lg:text-xl md:text-base text-xl font-semibold text-[#FF3811]'>{food_name}</p>
+          <p className="text-[#A2A2A2]">Email: {email}</p>
+          {/* <p className='text-[#A2A2A2]'>Name: {customerName}</p> */}
           <p className='text-[#A2A2A2]'>Price: {price}</p>
           <p className='text-[#A2A2A2]'>Quantity: {quantity}</p>
         </div>
-        </div>
-        {/* right div */}
-        <div className='flex items-center gap-4 pr-2'>
-        <div>
+
+        {/*right container for date and update */}
+        <div className="flex items-center lg:justify-end md:justify-end justify-between gap-4 p-2 md:p-0 lg:p-0 ">
           <p>Date: {date}</p>
-        </div>
-        <Link to={`/updateAddedFood/${_id}`}><img className='w-[2rem] hover: hover:bg-black hover:opacity-70' src={edit} alt="" />
-        </Link >
+
+          <Link to={`/updateAddedFood/${_id}`}>
+            <img
+              className="w-[2rem] hover: hover:bg-black hover:opacity-70"
+              src={edit}
+              alt=""
+            />
+          </Link>
+          {/* button for just mobile responsive */}
+          <button
+            onClick={() => handleDelete(_id)}
+            className=" btn-circle btn-sm opacity-50 hover:opacity-70 bg-black text-white px-3 visible md:hidden lg:hidden"
+          >
+            X
+          </button>
         </div>
       </div>
     </div>
-    );
+  );
 };
 
 export default AddedFoodList;
